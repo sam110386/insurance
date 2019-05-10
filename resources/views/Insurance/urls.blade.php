0a0da1afb54a64c9900a1e94e7f9e4a681d21bbe
semanticglobal <sgstest2505@gmail.com>
2019-05-10 12:17:44 +0530

    Thank you page changes

--- resources/views/Insurance/urls.blade.php
+++ resources/views/Insurance/urls.blade.php
@@ -3,9 +3,10 @@
 <div class="p-5">
 	<div class="container">
 		<div class="row">
-			<div class="col-12">
-				<div class="list-group">
-					<h1>Thank you for contact to us. We will contact you shortly</h1>
+			<div class="col-12 text-center">
+				<h2 class="mb-3">Thank you for your quote request. One of our agents will contact you shortly.</h2>
+				<a href="/" class="btn btn-warning text-white">Start New Quote</a>
+				<!--div class="list-group">
 					<a href="#" class="list-group-item list-group-item-action">
 						Cras justo odio
 					</a>
@@ -13,7 +14,7 @@
 					<a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
 					<a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
 					<a href="#" class="list-group-item list-group-item-action disabled">Vestibulum at eros</a>
-				</div>
+				</div-->
 			</div>
 		</div>
 	</div>
