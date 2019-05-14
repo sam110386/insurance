d66278c00cb8d468ed06d3d089c75152391730a7
semanticglobal <sgstest2505@gmail.com>
2019-05-14 19:12:08 +0530

    New Lead Mail changes

--- app/Http/Controllers/LeadsController.php
+++ app/Http/Controllers/LeadsController.php
@@ -15,10 +15,6 @@ class LeadsController extends Controller
 		$data['carMakes'] = CommonMethod::getCarMakes();
 		$data['carModels'] = CommonMethod::getModels();
 		$data['states'] = CommonMethod::getStates();
-		
-		$data['makes'] = ["ACURA","ALFA ROMEO","ASTON MARTIN","AUDI","BENTLEY","BMW",
-		"BUICK","CADILLAC","CHEVROLET","CHRYSLER","DODGE","FERRARI","FIAT","FORD"];
-		$data['models'] = ["Dodge Challenger","Dodge Challenger GT","Dodge Challenger SRT","Dodge Challenger SRT8","Dodge Charger","Dodge Charger AWD","Dodge Charger SRT","Dodge Charger SRT8","Dodge Dart","Dodge Dart Aero","Dodge Dart GT","Dodge Durango AWD","Dodge Durango RWD","Dodge Durango SRT AWD","Dodge Grand Caravan","Dodge Journey","Dodge Journey AWD","Dodge Viper","Dodge Viper SRT"];
 		$data['insuranceComp'] = ["21st Century","AIG","Allstate","Country Financial","Esurance","Farmers Ins","Geico","Liberty Mutual","MetLife","Nationwide","Progressive","State Farm","Other"];
 		return view('Lead.new',$data);
 	}
@@ -27,8 +23,7 @@ class LeadsController extends Controller
 		$lead = $request->all();
 		$ip = $_SERVER['REMOTE_ADDR'];
 		$city = CommonMethod::getZipcodeInfo($lead['zipcode']);
-		$ignoreFields =["_token","first_name","last_name","street","zipcode","phone","email","dob-month","dob-date","dob-year","gender","dl1","state1","first_name2","last_name2","dob2-month","dob2-date","dob2-year","gender-2","dl2","state2","first_name3","last_name3","dob3-month","dob3-date","dob3-year","gender-3","dl3","state3","first_name4","last_name4","dob4-month","dob4-date","dob4-year","gender-4","dl4","state4","first_name5","last_name5","dob5-month","dob5-date","dob5-year","gender-5","dl5","state5","vehicle-year","year","make","make-other","make-select","model1-other","model-1","vin1","ownership-vehicle-1","primary-use-vehicle-1","miles-driven-per-year-vehicle-1","vehicle-year-2","vehicle2-year","vehicle2-make","vehicle2-make-other","vehicle2-make-select","model2-other","model-2","vin2","ownership-vehicle-2","primary-use-vehicle-2","miles-driven-per-year-vehicle-2","vehicle-year-3","vehicle3-year","vehicle3-make","vehicle3-make-other","vehicle3-make-select","model3-other","model-3","vin3","ownership-vehicle-3","primary-use-vehicle-3","miles-driven-per-year-vehicle-3","vehicle-year-4","vehicle4-year","vehicle4-make","vehicle4-make-other","vehicle4-make-select","model4-other","model-4","vin4","ownership-vehicle-4","primary-use-vehicle-4","miles-driven-per-year-vehicle-4","vehicle-year-5","vehicle5-year","vehicle5-make","vehicle5-make-other","vehicle5-make-select","model5-other","model-5","vin5","ownership-vehicle-5","primary-use-vehicle-5","miles-driven-per-year-vehicle-5","vehicle2","vehicle3","vehicle4","vehicle5"];
-		$data = ['ignoreFields' => $ignoreFields, 'lead' => $lead,'ip' => $ip, 'city' => $city, 'states' => CommonMethod::getStates()];
+		$data = ['lead' => $lead,'ip' => $ip, 'city' => $city, 'states' => CommonMethod::getStates()];
 		\Mail::send('Emails.Lead.new', $data,
 		function ($message) {
 			$message->to('masisdavidian@gmail.com')->subject('New Lead - Insurance');
--- resources/views/Insurance/view.blade.php
+++ resources/views/Insurance/view.blade.php
@@ -1,55 +1,65 @@
 <style>
-.lead,h2 {
-  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-  margin-bottom: 10px;
-}
+	.table{max-width: 100%; overflow-x: auto; margin-bottom: 10px; }
+	.lead,h2 {
+		font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+		border-collapse: collapse;
+		width: 100%;
+		margin-bottom: 10px;
+	}
 
-.lead td, .lead th {
-  border: 1px solid #ddd;
-  padding: 8px;
-}
-
-.lead tr:nth-child(even){background-color: #f2f2f2;}
-
-.lead tr:hover {background-color: #ddd;}
-
-.lead th {
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  background-color: #4CAF50;
-  color: white;
-}
+	.lead td, .lead th {
+		border: 1px solid #ddd;
+		padding: 8px;
+	}
+	.lead tr:nth-child(even){background-color: #f2f2f2;}
+	.lead tr:hover {background-color: #ddd;}
+	.lead th {
+		padding-top: 12px;
+		padding-bottom: 12px;
+		text-align: left;
+		background-color: #4CAF50;
+		color: white;
+	}
+	.lead th span{font-weight: normal;}
+	.lead tr.bg-red{background-color: #f44336;}
+	.lead tr.bg-red td{color: #FFF;}
 </style>
-<table class="lead">
-	<tr><th colspan="2">CONTACT DETAILS</th></tr>
-	<tr><td><strong>Name</strong></td><td>{{$lead['first_name']}} {{$lead['last_name']}}</td></tr>
-	<tr><td><strong>Street</strong></td><td>{{$lead['street']}}</td></tr>
-	<tr><td><strong>City</strong></td><td>{{$city}},California,{{$lead['zipcode']}}</td></tr>
-	<tr><td><strong>Phone</strong></td><td>{{$lead['phone']}}</td></tr>
-	<tr><td><strong>Email</strong></td><td>{{$lead['email']}}</td></tr>
-</table>
-<table class="lead">
-	<tr><th colspan="6">DRIVER DETAILS</th></tr>
-	<tr>
-		<td><strong>#</strong></td>
-		<td><strong>Name</strong></td>
-		<td><strong>Date of Birth</strong></td>
-		<td><strong>Gender</strong></td>
-		<td><strong>Drivers License Number</strong></td>
-		<td><strong>State</strong></td>
-	</tr>
-	<tr>
-		<td>1.</td>
-		<td>{{$lead['first_name']}} {{$lead['last_name']}}</td> 
-		<td>{{$lead['dob-month']}}-{{$lead['dob-date']}}-{{$lead['dob-year']}}</td>
-		<td>{{$lead['gender']}}</td>
-		<td>{{$lead['dl1']}}</td>
-		<td>{{$states[$lead['state1']]}}</td>
-	</tr>
-	@if($lead['first_name2'])
+<div class="table">
+	<table class="lead">
+		<tr><th colspan="2">CONTACT DETAILS</th></tr>
+		<tr><td><strong>Name</strong></td><td>{{$lead['first_name']}} {{$lead['last_name']}}</td></tr>
+		<tr><td><strong>Street</strong></td><td>{{$lead['street']}}</td></tr>
+		<tr><td><strong>City</strong></td><td>{{$city}},California,{{$lead['zipcode']}}</td></tr>
+		<tr><td><strong>Phone</strong></td><td>{{$lead['phone']}}</td></tr>
+		<tr><td><strong>Email</strong></td><td>{{$lead['email']}}</td></tr>
+
+		<tr><td><strong>Married</strong></td><td>@if($lead['married']){{"Yes"}}@else{{"No"}}@endif</td></tr>
+		<tr><td><strong>Children</strong></td><td>@if(isset($lead['children']) && $lead['children']){{"Yes"}}@else{{"No"}}@endif</td></tr>
+		<tr><td><strong>Homeowner</strong></td><td>{{$lead['homeowner']}}</td></tr>
+		<tr><td><strong>Bundled</strong></td><td>@if($lead['bundled']){{"Yes"}}@else{{"No"}}@endif</td></tr>
+	</table>
+</div>
+<!-- Drivers Details -->
+<div class="table">
+	<table class="lead">
+		<tr><th colspan="6">DRIVER DETAILS</th></tr>
+		<tr>
+			<td><strong>#</strong></td>
+			<td><strong>Name</strong></td>
+			<td><strong>Date of Birth</strong></td>
+			<td><strong>Gender</strong></td>
+			<td><strong>Drivers License Number</strong></td>
+			<td><strong>State</strong></td>
+		</tr>
+		<tr>
+			<td>1.</td>
+			<td>{{$lead['first_name']}} {{$lead['last_name']}}</td> 
+			<td>{{$lead['dob-month']}}-{{$lead['dob-date']}}-{{$lead['dob-year']}}</td>
+			<td>{{$lead['gender']}}</td>
+			<td>{{$lead['dl1']}}</td>
+			<td>{{$states[$lead['state1']]}}</td>
+		</tr>
+		@if($lead['first_name2'])
 		<tr>
 			<td>2.</td>
 			<td>{{$lead['first_name2']}} {{$lead['last_name2']}}</td> 
@@ -58,8 +68,8 @@
 			<td>{{$lead['dl2']}}</td>
 			<td>{{$states[$lead['state2']]}}</td>
 		</tr>
-	@endif
-	@if($lead['first_name3'])
+		@endif
+		@if($lead['first_name3'])
 		<tr>
 			<td>3.</td>
 			<td>{{$lead['first_name3']}} {{$lead['last_name3']}}</td> 
@@ -68,8 +78,8 @@
 			<td>{{$lead['dl3']}}</td>
 			<td>{{$states[$lead['state3']]}}</td>
 		</tr>
-	@endif
-	@if($lead['first_name4'])
+		@endif
+		@if($lead['first_name4'])
 		<tr>
 			<td>4.</td>
 			<td>{{$lead['first_name4']}} {{$lead['last_name4']}}</td> 
@@ -78,8 +88,8 @@
 			<td>{{$lead['dl4']}}</td>
 			<td>{{$states[$lead['state4']]}}</td>
 		</tr>
-	@endif
-	@if($lead['first_name5'])
+		@endif
+		@if($lead['first_name5'])
 		<tr>
 			<td>5.</td>
 			<td>{{$lead['first_name5']}} {{$lead['last_name5']}}</td> 
@@ -88,189 +98,249 @@
 			<td>{{$lead['dl5']}}</td>
 			<td>{{$states[$lead['state5']]}}</td>
 		</tr>
-	@endif	
-</table>
-
-<table class="lead">
-	<tr><th colspan="8">VEHICLE DETAILS</th></tr>
-	<tr>
-		<td><strong>#</strong></td>
-		<td><strong>Year</strong></td>
-		<td><strong>Make</strong></td>
-		<td><strong>Model</strong></td>
-		<td><strong>Vin</strong></td>
-		<td><strong>Ownership</strong></td>
-		<td><strong>Uses</strong></td>
-		<td><strong>Mileage</strong></td>
-	</tr>
-	<tr>
-		<td>1.</td>
-		<td>
-			@if($lead['vehicle-year'])
+		@endif	
+	</table>
+</div>
+<div class="table">
+	<!-- Vehicles Details -->
+	<table class="lead">
+		<tr><th colspan="8">VEHICLE DETAILS</th></tr>
+		<tr>
+			<td><strong>#</strong></td>
+			<td><strong>Year</strong></td>
+			<td><strong>Make</strong></td>
+			<td><strong>Model</strong></td>
+			<td><strong>Vin</strong></td>
+			<td><strong>Ownership</strong></td>
+			<td><strong>Uses</strong></td>
+			<td><strong>Mileage</strong></td>
+		</tr>
+		<tr>
+			<td>1.</td>
+			<td>
+				@if($lead['vehicle-year'])
 				{{$lead['vehicle-year']}}
-			@else
+				@else
 				{{$lead['year']}}
-			@endif
-		</td>
-		<td>
-			@if($lead['make-other'])
+				@endif
+			</td>
+			<td>
+				@if($lead['make-other'])
 				{{$lead['make-other']}}
-			@elseif($lead['make-select'])
+				@elseif($lead['make-select'])
 				{{$lead['make-select']}}
-			@else
+				@else
 				{{$lead['make']}}
-			@endif
-		</td>
-		<td>
-			@if($lead['model1-other'])
+				@endif
+			</td>
+			<td>
+				@if($lead['model1-other'])
 				{{$lead['model1-other']}}
-			@else
+				@else
 				{{$lead['model-1']}}
-			@endif
-		</td>
-		<td>{{$lead['vin1']}}</td>
-		<td>{{$lead['ownership-vehicle-1']}}</td>
-		<td>{{$lead['primary-use-vehicle-1']}}</td>
-		<td>{{$lead['miles-driven-per-year-vehicle-1']}}</td>
-	</tr>
-	@if($lead['vehicle2'] && $lead['miles-driven-per-year-vehicle-2'])
-	<tr>
-		<td>2.</td>
-		<td>
-			@if($lead['vehicle-year-2'])
+				@endif
+			</td>
+			<td>{{$lead['vin1']}}</td>
+			<td>{{$lead['ownership-vehicle-1']}}</td>
+			<td>{{$lead['primary-use-vehicle-1']}}</td>
+			<td>{{$lead['miles-driven-per-year-vehicle-1']}}</td>
+		</tr>
+		@if($lead['vehicle2'] && $lead['miles-driven-per-year-vehicle-2'])
+		<tr>
+			<td>2.</td>
+			<td>
+				@if($lead['vehicle-year-2'])
 				{{$lead['vehicle-year-2']}}
-			@else
+				@else
 				{{$lead['vehicle2-year']}}
-			@endif
-		</td>
-		<td>
-			@if($lead['vehicle2-make-other'])
+				@endif
+			</td>
+			<td>
+				@if($lead['vehicle2-make-other'])
 				{{$lead['vehicle2-make-other']}}
-			@elseif($lead['vehicle2-make-select'])
+				@elseif($lead['vehicle2-make-select'])
 				{{$lead['vehicle2-make-select']}}
-			@else
+				@else
 				{{$lead['vehicle2-make']}}
-			@endif
-		</td>
-		<td>
-			@if($lead['model2-other'])
+				@endif
+			</td>
+			<td>
+				@if($lead['model2-other'])
 				{{$lead['model2-other']}}
-			@else
+				@else
 				{{$lead['model-2']}}
-			@endif
-		</td>
-		<td>{{$lead['vin2']}}</td>
-		<td>{{$lead['ownership-vehicle-2']}}</td>
-		<td>{{$lead['primary-use-vehicle-2']}}</td>
-		<td>{{$lead['miles-driven-per-year-vehicle-2']}}</td>
-	</tr>
-	@endif
-	@if(isset($lead['vehicle3']) && $lead['vehicle3']  && $lead['miles-driven-per-year-vehicle-3'])
-	<tr>
-		<td>3.</td>
-		<td>
-			@if($lead['vehicle-year-3'])
+				@endif
+			</td>
+			<td>{{$lead['vin2']}}</td>
+			<td>{{$lead['ownership-vehicle-2']}}</td>
+			<td>{{$lead['primary-use-vehicle-2']}}</td>
+			<td>{{$lead['miles-driven-per-year-vehicle-2']}}</td>
+		</tr>
+		@endif
+		@if(isset($lead['vehicle3']) && $lead['vehicle3']  && $lead['miles-driven-per-year-vehicle-3'])
+		<tr>
+			<td>3.</td>
+			<td>
+				@if($lead['vehicle-year-3'])
 				{{$lead['vehicle-year-3']}}
-			@else
+				@else
 				{{$lead['vehicle3-year']}}
-			@endif
-		</td>
-		<td>
-			@if($lead['vehicle3-make-other'])
+				@endif
+			</td>
+			<td>
+				@if($lead['vehicle3-make-other'])
 				{{$lead['vehicle3-make-other']}}
-			@elseif($lead['vehicle3-make-select'])
+				@elseif($lead['vehicle3-make-select'])
 				{{$lead['vehicle3-make-select']}}
-			@else
+				@else
 				{{$lead['vehicle3-make']}}
-			@endif
-		</td>
-		<td>
-			@if($lead['model3-other'])
+				@endif
+			</td>
+			<td>
+				@if($lead['model3-other'])
 				{{$lead['model3-other']}}
-			@else
+				@else
 				{{$lead['model-3']}}
-			@endif
-		</td>
-		<td>{{$lead['vin3']}}</td>
-		<td>{{$lead['ownership-vehicle-3']}}</td>
-		<td>{{$lead['primary-use-vehicle-3']}}</td>
-		<td>{{$lead['miles-driven-per-year-vehicle-3']}}</td>
-	</tr>
-	@endif
-	@if(isset($lead['vehicle4']) && $lead['vehicle4']  && $lead['miles-driven-per-year-vehicle-4'])
-	<tr>
-		<td>4.</td>
-		<td>
-			@if($lead['vehicle-year-4'])
+				@endif
+			</td>
+			<td>{{$lead['vin3']}}</td>
+			<td>{{$lead['ownership-vehicle-3']}}</td>
+			<td>{{$lead['primary-use-vehicle-3']}}</td>
+			<td>{{$lead['miles-driven-per-year-vehicle-3']}}</td>
+		</tr>
+		@endif
+		@if(isset($lead['vehicle4']) && $lead['vehicle4']  && $lead['miles-driven-per-year-vehicle-4'])
+		<tr>
+			<td>4.</td>
+			<td>
+				@if($lead['vehicle-year-4'])
 				{{$lead['vehicle-year-4']}}
-			@else
+				@else
 				{{$lead['vehicle4-year']}}
-			@endif
-		</td>
-		<td>
-			@if($lead['vehicle4-make-other'])
+				@endif
+			</td>
+			<td>
+				@if($lead['vehicle4-make-other'])
 				{{$lead['vehicle4-make-other']}}
-			@elseif($lead['vehicle4-make-select'])
+				@elseif($lead['vehicle4-make-select'])
 				{{$lead['vehicle4-make-select']}}
-			@else
+				@else
 				{{$lead['vehicle4-make']}}
-			@endif
-		</td>
-		<td>
-			@if($lead['model4-other'])
+				@endif
+			</td>
+			<td>
+				@if($lead['model4-other'])
 				{{$lead['model4-other']}}
-			@else
+				@else
 				{{$lead['model-4']}}
-			@endif
-		</td>
-		<td>{{$lead['vin4']}}</td>
-		<td>{{$lead['ownership-vehicle-4']}}</td>
-		<td>{{$lead['primary-use-vehicle-4']}}</td>
-		<td>{{$lead['miles-driven-per-year-vehicle-4']}}</td>
-	</tr>
-	@endif
-	@if(isset($lead['vehicle5']) && $lead['vehicle5']  && $lead['miles-driven-per-year-vehicle-5'])
-	<tr>
-		<td>5.</td>
-		<td>
-			@if($lead['vehicle-year-5'])
+				@endif
+			</td>
+			<td>{{$lead['vin4']}}</td>
+			<td>{{$lead['ownership-vehicle-4']}}</td>
+			<td>{{$lead['primary-use-vehicle-4']}}</td>
+			<td>{{$lead['miles-driven-per-year-vehicle-4']}}</td>
+		</tr>
+		@endif
+		@if(isset($lead['vehicle5']) && $lead['vehicle5']  && $lead['miles-driven-per-year-vehicle-5'])
+		<tr>
+			<td>5.</td>
+			<td>
+				@if($lead['vehicle-year-5'])
 				{{$lead['vehicle-year-5']}}
-			@else
+				@else
 				{{$lead['vehicle5-year']}}
-			@endif
-		</td>
-		<td>
-			@if($lead['vehicle5-make-other'])
+				@endif
+			</td>
+			<td>
+				@if($lead['vehicle5-make-other'])
 				{{$lead['vehicle5-make-other']}}
-			@elseif($lead['vehicle5-make-select'])
+				@elseif($lead['vehicle5-make-select'])
 				{{$lead['vehicle5-make-select']}}
-			@else
+				@else
 				{{$lead['vehicle5-make']}}
-			@endif
-		</td>
-		<td>
-			@if($lead['model5-other'])
+				@endif
+			</td>
+			<td>
+				@if($lead['model5-other'])
 				{{$lead['model5-other']}}
-			@else
+				@else
 				{{$lead['model-5']}}
-			@endif
-		</td>
-		<td>{{$lead['vin5']}}</td>
-		<td>{{$lead['ownership-vehicle-5']}}</td>
-		<td>{{$lead['primary-use-vehicle-5']}}</td>
-		<td>{{$lead['miles-driven-per-year-vehicle-5']}}</td>
-	</tr>
-	@endif
-</table>
-
-
-<h2>ALL DATA</h2>
-<table class="lead">
-	<tr><th>FIELD</th><th>VALUE</th></tr>
-	<tr><td><strong>Client IP</strong></th><td>{{$ip}}</td></tr>
-	@foreach($lead as $key => $value)
-	@if(!in_array($key,$ignoreFields))
-	<tr><td><strong>{{$key}}</strong></td><td>{{$value}}</td></tr>
-	@endif
-	@endforeach
-</table>
+				@endif
+			</td>
+			<td>{{$lead['vin5']}}</td>
+			<td>{{$lead['ownership-vehicle-5']}}</td>
+			<td>{{$lead['primary-use-vehicle-5']}}</td>
+			<td>{{$lead['miles-driven-per-year-vehicle-5']}}</td>
+		</tr>
+		@endif
+	</table>
+</div>
+<!-- Coverage Details -->
+<div class="table">
+	<table class="lead">
+		<tr><th colspan="2">COVERAGE DETAILS:</th></tr>
+		<tr><td><strong>Liability</strong></td><td>NA</td></tr>
+		<tr><td><strong>Body Injury ($)</strong></td><td>{{$lead['bodily-injury']}}</td></tr>
+		<tr><td><strong>Deduct ($)</strong></td><td>{{$lead['deductible']}}</td></tr>
+		<tr><td><strong>Medical ($)</strong></td><td>{{$lead['medical']}}</td></tr>
+		<tr><td><strong>Towing</strong></td><td>@if($lead['towing']){{"Yes"}}@else{{"No"}}@endif</td></tr>
+		<tr><td><strong>Uninsured</strong></td><td>@if($lead['uninsured']){{"Yes"}}@else{{"No"}}@endif</td></tr>
+		<tr><td><strong>Rental</strong></td><td>@if($lead['rental']){{"Yes"}}@else{{"No"}}@endif</td></tr>
+	</table>
+</div>
+<!-- History Details -->
+<div class="table">
+	<table class="lead">
+		<tr><th colspan="2">HISTORY DETAILS</th></tr>
+		<tr>
+			<td><strong>Previous Insurance</strong></td>
+			<td>@if($lead['previous-insurance']){{"Yes"}}@else{{"No"}}@endif</td>
+		</tr>
+		<tr>
+			<td><strong>Current Insurance</strong></td>
+			<td>@if(isset($lead['current-insurance'])){{$lead['current-insurance']}}@else{{"NA"}}@endif</td>
+		</tr>
+		<tr>
+			<td><strong>Duration</strong></td>
+			<td>@if(isset($lead['current-insurance-duration'])){{$lead['current-insurance-duration']}} Years @else{{"NA"}}@endif</td>
+		</tr>
+		<tr @if($lead["at_fault"]) class="bg-red" @endif>
+			<td><strong>At Fault</strong></td>
+			<td>@if($lead['at_fault']){{"Yes"}}@else{{"No"}}@endif</td>
+		</tr>
+		<tr @if($lead["tickets"]) class="bg-red" @endif>
+			<td><strong>Tickets</strong></td>
+			<td>@if($lead['tickets']){{"Yes"}}@else{{"No"}}@endif</td>
+		</tr>
+		<tr @if($lead["dui"]) class="bg-red" @endif>
+			<td><strong>DUI</strong></td>
+			<td class="bg-red">@if($lead['dui']){{"Yes"}}@else{{"No"}}@endif</td>
+		</tr>
+	</table>
+</div>
+<!-- Preference Details: -->
+<div class="table">
+	<table class="lead">
+		<tr><th colspan="2">PREFERENCE DETAILS:</th></tr>
+		<tr><td><strong>Quality Provides</strong></td><td>{!! ucwords(str_replace('-', ' ', $lead['quality'])) !!}</td></tr>
+		<tr><td><strong>Agent In Person</strong></td><td>@if($lead['agent_in_person']){{"Yes"}}@else{{"No"}}@endif</td></tr>
+		<tr><td><strong>Referrer</strong></td><td>{{$lead['referrer']}}</td></tr>
+		<tr><td><strong>Referrer Name</strong></td><td>{{$lead['referrer_name']}}</td></tr>
+	</table>
+</div>
+<div class="table">
+	<table class="lead">
+		<th>
+			<strong>IP:</strong>
+			<span>{{$ip}}</span>
+		</th>
+		<th>
+			<strong>DATE:</strong>
+			<span>{{ now()->format('Y M d') }}</span>
+		</th>
+		<th>
+			<strong>TIME:</strong>
+			<span>{{ now()->format('h:i:s A') }}</span>
+		</th>
+	</table>
+</div>
