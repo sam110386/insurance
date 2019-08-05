624259676af54d5fe8099c37730ad489db9c5a11
semanticglobal <sgstest2505@gmail.com>
2019-08-05 17:57:23 +0530

    TnC checkbox size

--- resources/views/Lead/new.blade.php
+++ resources/views/Lead/new.blade.php
@@ -3590,7 +3590,7 @@
 		<div class="row">
 			<div class="col-md-10 offset-md-1">
 				<p>
-					<a data-href="dl5" class="change-question prev text-primary btn btn-warning btn-sm"> 
+					<a data-href="name5" class="change-question prev text-primary btn btn-warning btn-sm"> 
 							<i class="fa fa-angle-left"></i> Previous Question
 					</a>
 				</p>
@@ -3639,7 +3639,7 @@
 					--}}
 					<div class="col-12">
 						<label class="tnc-text">
-							<div class="custom-control custom-checkbox float-left mr-2">
+							<div class="custom-control custom-checkbox float-left mr-3">
 							  <input type="checkbox" class="custom-control-input" id="TnC">
 							  <label class="custom-control-label" for="TnC"></label>
 							</div>
