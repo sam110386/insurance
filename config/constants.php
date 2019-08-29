800833598c5c84a1b092d9e4c44e4c493a5b0696
semanticglobal <sgstest2505@gmail.com>
2019-08-29 19:05:34 +0530

    User group assignment while creating new user.Emails notification as well

--- config/constants.php
+++ config/constants.php
@@ -1,5 +1,4 @@
 <?php 
-dd(env("APP_ENV"));
 	return [
 		'admin_email' => (env("APP_ENV") == 'local') ? "vbmourya123@gmail.com" : "allensaraf@gmail.com",
 		'admin_bcc_email' => (env("APP_ENV") == 'local') ? "sgstest2505@gmail.com" : "masisdavidian@gmail.com"
